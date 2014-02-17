5d1888656b266d60140683c38a0ca752e257038b
Derrick Ampy <dsampy@gmail.com>
2014-02-17 17:33:26 -0500

    Updated install Puppet classes to download artifacts

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,3 +1,8 @@
 class openidm::install {
-
+	wget::fetch { "Download OpenIDM":
+	  source      => 'http://maven.forgerock.org/repo/releases/org/forgerock/openidm/openidm-zip/2.1.0/openidm-zip-2.1.0.zip',
+	  destination => '/tmp/openidm-2.1.0.zip',
+	  timeout     => 0,
+	  verbose     => false,
+	}
 }
