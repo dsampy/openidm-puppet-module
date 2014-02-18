b4b15ef611a32b2d420c3ca2aca8e7cd2335604a
Derrick Ampy <dsampy@gmail.com>
2014-02-17 20:26:27 -0500

    Updated Puppet install classes to use nexus repository

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,6 +1,6 @@
 class openidm::install {
 	wget::fetch { "Download OpenIDM":
-	  source      => 'http://maven.forgerock.org/repo/releases/org/forgerock/openidm/openidm-zip/2.1.0/openidm-zip-2.1.0.zip',
+	  source      => 'http://10.0.2.2:8081/nexus/service/local/repositories/public/content/org/forgerock/openidm/openidm-zip/2.1.0/openidm-zip-2.1.0.zip',
 	  destination => '/tmp/openidm-2.1.0.zip',
 	  timeout     => 0,
 	  verbose     => false,
